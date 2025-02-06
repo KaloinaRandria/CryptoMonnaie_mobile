9fb8090dd93557f61866f54226f045784fbccec7
Kaloina RANDRIAMBOLOLONA <kaloinarandria@outlook.fr>
2025-02-06 14:53:31 +0300

    Delete JetonUtilisateur.js

--- models/JetonUtilisateur.js
+++
@@ -1,9 +0,0 @@
-export default class JetonUtilisateur {
-    constructor(id , nom , mail , mdp , jeton) {
-        this.id = id;
-        this.nom = nom;
-        this.mail = mail;
-        this.mdp = mdp;
-        this.jeton = jeton;
-    }
-}
