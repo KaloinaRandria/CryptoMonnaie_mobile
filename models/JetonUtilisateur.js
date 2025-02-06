1c937d42fca16c2efafd22d78d6c3dbb32ff8638
Kaloina RANDRIAMBOLOLONA <kaloinarandria@outlook.fr>
2025-02-06 14:44:17 +0300

    Create JetonUtilisateur.js

---
+++ models/JetonUtilisateur.js
@@ -0,0 +1,9 @@
+export default class JetonUtilisateur {
+    constructor(id , nom , mail , mdp , jeton) {
+        this.id = id;
+        this.nom = nom;
+        this.mail = mail;
+        this.mdp = mdp;
+        this.jeton = jeton;
+    }
+}
